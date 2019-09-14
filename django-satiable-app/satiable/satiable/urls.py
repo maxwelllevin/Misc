2fc9e486155d0fd5bd9e872d30c59443c1de521a
Maxwell <msl@lclark.edu>
2019-09-14 15:15:51 -0700

    Added hello world shop page

--- django-satiable-app/satiable/satiable/urls.py
+++ django-satiable-app/satiable/satiable/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('shop.urls')),  # include the urls.py file from shop/. Set path part ('') to whatever -- 'shop/'
     path('admin/', admin.site.urls),
 ]
--- django-satiable-app/satiable/shop/views.py
+++ django-satiable-app/satiable/shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    """The index page for shop"""
+    return HttpResponse("Hello world, this is the satiable landing page.")
