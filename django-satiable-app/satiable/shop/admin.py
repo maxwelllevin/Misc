33f902446706eaa63f7e349dbdfff91e46fc819c
Maxwell <msl@lclark.edu>
2019-09-15 12:56:14 -0700

    Added models for store items and associated item images

--- django-satiable-app/satiable/shop/admin.py
+++ django-satiable-app/satiable/shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Item, ItemImage
+
 # Register your models here.
+admin.site.register(Item)
--- django-satiable-app/satiable/shop/models.py
+++ django-satiable-app/satiable/shop/models.py
@@ -1,3 +1,31 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Item(models.Model):
+    """An item is something that can be or was for sale in the shop."""
+    title = models.CharField(max_length=140)
+    description = models.CharField(max_length=400)
+    price = models.IntegerField()  # TODO: Let this take on decimal values
+    num_available = models.IntegerField()  # Number of available copies
+    num_purchased = models.IntegerField(default=0)  # Number of copies that have been sold
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=5)
+
+
+class ItemImage(models.Model):
+    """An ItemImage is a picture of a particular item. There can be many pictures of a single item."""
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    alt_text = models.CharField(max_length=100)
+    img_path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.alt_text
+
