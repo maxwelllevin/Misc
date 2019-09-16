5bad2f386b12501f3e620a64bab7f38fa22ea0b1
Maxwell <maxwellevin@users.noreply.github.com>
2019-09-16 13:15:39 -0700

    Create 7. Reverse Integer.java

---
+++ LeetCode/7. Reverse Integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        int dig = 0, rev = 0;
+        while (x != 0) {
+            dig = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && dig > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && dig < -8)) return 0;
+            rev = rev*10 + dig;
+        }
+        return rev;
+    }
+}
