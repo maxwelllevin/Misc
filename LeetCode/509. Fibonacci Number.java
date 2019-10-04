0023c8513023e93e018b7318a3665f69414ca13c
Maxwell <maxwellevin@users.noreply.github.com>
2019-10-04 12:33:55 -0700

    Create 509. Fibonacci Number.java

---
+++ LeetCode/509. Fibonacci Number.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int fib(int N) {
+        double goldenRatio = (1 + Math.sqrt(5)) / 2;
+        return (int) Math.round(Math.pow(goldenRatio, N) / Math.sqrt(5));
+    }
+}
