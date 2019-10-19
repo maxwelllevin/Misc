224a7b137ad533a5640abeca2a4ee46cd12b9f40
Maxwell <maxwellevin@users.noreply.github.com>
2019-10-18 21:15:01 -0700

    Create 206. Reverse Linked List.java

---
+++ LeetCode/206. Reverse Linked List.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+import java.util.ArrayList;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
